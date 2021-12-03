f63518f3e5b00b1095825f951def5f4777de0284
Peter Fajner <pfaj@dal.ca>
2021-12-03 17:24:33 -0400

    Continue refactoring to use new classes

--- src/ca/pfaj/pentameter/Phrase.java
+++ src/ca/pfaj/pentameter/Phrase.java
@@ -3,6 +3,10 @@ package ca.pfaj.pentameter;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.google.common.collect.Sets.cartesianProduct;
 
 /**
  * A list of words
@@ -58,17 +62,73 @@ public record Phrase(List<Word> words, Dictionary dictionary) {
     }
 
     /**
+     * Get all unique pronounciations of this phrase
+     * @return all unique pronounciations of this phrase
+     */
+    public Set<PronouncedPhrase> uniqueProunounciations() {
+        /*
+         * The set of all unique pronounciations of this phrase is equal to the
+         * cartesian product of the unique pronounciations of all the words.
+         * ex: A phrase with three words (A, B, C). A has two pronounciations (i and ii),
+         *     B has two pronounciations (i and ii), and C has three
+         *     pronounciations (i, ii, iii). The possible unique pronounciations are
+         *     [Ai  ]       [Bi  ]       [Ci  ]
+         *     [Aii ]   x   [Bii ]   x   [Cii ]
+         *                               [Ciii]
+         *     = [AiBiCi, AiBiCii, AiBiCiii, AiBiiCi, AiBiiCii, AiBiiCiii,
+         *        AiiBiCi, AiiBiCii, AiiBiCiii, AiiBiiCi, AiiBiiCii, AiiBiiCiii]
+         */
+        // get unique pronounciation for each word
+        List<Set<PronouncedWord>> uniqueWords = new LinkedList<>();
+        for (var word : words) {
+            uniqueWords.add(word.uniquePronounciations());
+        }
+        // get cartesian product of the unique pronounciations
+        return cartesianProduct(uniqueWords).stream().map(PronouncedPhrase::new).collect(Collectors.toSet());
+    }
+
+    /**
      * Colour the phrase, choosing a pronounciation that is iambic if possible.
      */
     public String colour() {
+        // get all unique pronounciations for this phrase
+        var pronounciationOptions = uniqueProunounciations();
+        // try to find iambic penameter, just iambic, or not iambic, in that order
+        PronouncedPhrase firstIambicPentameter = null;
+        PronouncedPhrase firstIambic = null;
+        PronouncedPhrase chosen = null;
+        for (var option : pronounciationOptions) {
+            if (option.isIambicPentameter()) {
+                firstIambicPentameter = option;
+                break;
+            } else if (option.isIambic()) {
+                firstIambic = option;
+            } else {
+                chosen = option;
+            }
+        }
+        if (firstIambicPentameter != null) {
+            chosen = firstIambicPentameter
+        } else if (firstIambic != null) {
+            chosen = firstIambic;
+        }
+        // colour the phrase
         throw new UnsupportedOperationException();
     }
 
     /**
-     * Get the phrase's iambic pronounciation, if it exists.
-     * @return a list of words with a single chosen pronounciation each
+     * Get the phrase's iambic pronounciation, if it exists. If multiple exist, choose one.
+     * @return the phrase's iambic pronounciation, if it exists
      */
     public Optional<PronouncedPhrase> getIambicPronounciation() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Get the phrase's iambic pentameter pronounciation, if it exists. If multiple exists, choose one.
+     * @return the phrase's iambic pentameter pronounciation, if it exists
+     */
+    public Optional<PronouncedPhrase> getIambicPentameterPronounciation() {
         throw new UnsupportedOperationException();
     }
 }
@@ -77,5 +137,15 @@ public record Phrase(List<Word> words, Dictionary dictionary) {
  * A phrase with a single, chosen pronounciation for all the words.
  */
 record PronouncedPhrase(List<PronouncedWord> words) {
+    public boolean isIambic() {
+        throw new UnsupportedOperationException();
+    }
 
+    public boolean isIambicPentameter() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String colour() {
+        throw new UnsupportedOperationException();
+    }
 }
--- src/ca/pfaj/pentameter/Word.java
+++ src/ca/pfaj/pentameter/Word.java
@@ -2,19 +2,26 @@ package ca.pfaj.pentameter;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * A word that can be pronounced in one or more ways.
  */
 public record Word(String name, Set<Pronounciation> pronounciationOptions) {
-
+    /**
+     * Get a set of PronouncedWords, representing each unique pronounciation of this word
+     * @return the unique pronounciations of this word
+     */
+    public Set<PronouncedWord> uniquePronounciations() {
+        return pronounciationOptions.stream().map(
+                opt -> new PronouncedWord(name, opt)).collect(Collectors.toSet());
+    }
 }
 
 /**
  * A word with one particular chosen pronounciation.
  */
-record PronouncedWord(String name, List<Stress> pronounciation) {
-
+record PronouncedWord(String name, Pronounciation pronounciation) {
 }
 
 /**
